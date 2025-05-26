ff3a4a6d239734a8e4d2b5f71c9d8a2308f55f8c
HakPyun <kjiz0503@gmail.com>
2025-05-26 12:29:20 +0900

    test : mcp를 활용한 로그인페이지 구현 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import LoginPage from './pages/LoginPage';
 
 function App() {
-  return <LoginPage />;
+  return (
+    <div style={{
+      minHeight: '100vh',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      background: '#f9fafb'
+    }}>
+      <LoginPage />
+    </div>
+  );
 }
 
 export default App;
