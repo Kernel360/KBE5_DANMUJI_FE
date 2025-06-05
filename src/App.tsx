297c8f2e8e56f5b6eb45caf8c04639549b5a17e6
ooyniz <ooyniz@gmail.com>
2025-06-05 18:37:22 +0900

    feat: 로그인페이지 사이드바 표시 제외 (#15)

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import {
   BrowserRouter as Router,
   Routes,
   Route,
-  Navigate,
+  useLocation,
 } from "react-router-dom";
 import { Sidebar } from "./layouts/Sidebar";
 import { Topbar } from "./layouts/Topbar";
@@ -15,28 +15,40 @@ import LoginPage from "./features/auth/pages/LoginPage";
 import ProjectPostPage from "./features/project/pages/ProjectPostPage";
 import { AppContainer, MainContent, PageContent } from "./App.styled";
 
+const LayoutWrapper = ({ children }: { children: React.ReactNode }) => {
+  const location = useLocation();
+  const isLoginPage = location.pathname === "/" || location.pathname === "/login";
+
+  if (isLoginPage) {
+    return <>{children}</>;
+  }
+
+  return (
+    <AppContainer>
+      <Sidebar />
+      <MainContent>
+        <Topbar />
+        <PageContent>{children}</PageContent>
+      </MainContent>
+    </AppContainer>
+  );
+};
+
 function App() {
   return (
     <Router>
-      <AppContainer>
-        <Sidebar />
-        <MainContent>
-          <Topbar />
-          <PageContent>
-            <Routes>
-              {/* <Route path="/" element={<Navigate to="/dashboard" />} /> */}
-              <Route path="/" element={<LoginPage />} />
-              <Route path="/login" element={<LoginPage />} />
-              <Route path="/dashboard" element={<DashboardPage />} />
-              <Route path="/company" element={<CompanyPage />} />
-              <Route path="/members" element={<MemberPage />} />
-              <Route path="/projects" element={<ProjectPage />} />
-              <Route path="/projects/posts" element={<ProjectPostPage />} />
-              <Route path="/dashboard2" element={<DashboardPage2 />} />
-            </Routes>
-          </PageContent>
-        </MainContent>
-      </AppContainer>
+      <LayoutWrapper>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
+          <Route path="/company" element={<CompanyPage />} />
+          <Route path="/members" element={<MemberPage />} />
+          <Route path="/projects" element={<ProjectPage />} />
+          <Route path="/projects/posts" element={<ProjectPostPage />} />
+          <Route path="/dashboard2" element={<DashboardPage2 />} />
+        </Routes>
+      </LayoutWrapper>
     </Router>
   );
 }
