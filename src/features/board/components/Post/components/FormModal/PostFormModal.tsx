0ab83a11abd158fdb0b8c69831300b8c40738e67
swlee <swo98@naver.com>
2025-06-20 18:41:24 +0900

    style: 단계 드롭다운 버튼 색 수정 (#105)

--- src/features/board/components/Post/components/FormModal/PostFormModal.tsx
+++ src/features/board/components/Post/components/FormModal/PostFormModal.tsx
@@ -750,7 +750,7 @@ const PostFormModal: React.FC<PostFormModalProps> = ({
                               ? "#fdb924"
                               : selectedStep?.projectStepStatus === "COMPLETED"
                               ? "#10b981"
-                              : "#6b7280"
+                              : "#3b82f6"
                           }
                           $isOpen={isStepDropdownOpen}
                           onClick={handleStepDropdownToggle}
@@ -790,7 +790,7 @@ const PostFormModal: React.FC<PostFormModalProps> = ({
                                     ? "#fdb924"
                                     : step.projectStepStatus === "COMPLETED"
                                     ? "#10b981"
-                                    : "#6b7280"
+                                    : "#3b82f6"
                                 }
                                 onClick={() => handleStepSelect(step.id)}
                               >
